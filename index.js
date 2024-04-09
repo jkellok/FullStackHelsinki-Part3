3ee02a74270a7a0ff92d583e62fc201287c49929
Juska Kellokumpu <juskakaro.kellokumpu@tuni.fi>
2024-04-09 13:05:33 +0300

    phonebook backend deployment

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('body', function(req, res) {
     return JSON.stringify(req.body)
@@ -104,7 +106,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
