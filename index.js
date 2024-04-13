a67c8c305084e60573708c224e6659d68a439292
Juska Kellokumpu <juskakaro.kellokumpu@tuni.fi>
2024-04-13 13:46:55 +0300

    Phonebook/3.21 done

--- index.js
+++ index.js
@@ -49,18 +49,6 @@ app.delete('/api/persons/:id', (request, response, next) => {
 app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
-    if (body.name === "") {
-        return response.status(400).json({
-            error: 'name missing'
-        })
-    }
-
-    if (body.number === "") {
-        return response.status(400).json({
-            error: 'number missing'
-        })
-    }
-
     const person = new Person({
         name: body.name,
         number: body.number
@@ -74,14 +62,9 @@ app.post('/api/persons', (request, response, next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
+    const { name, number } = request.body
 
-    const person = {
-        name: body.name,
-        number: body.number,
-    }
-
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    Person.findByIdAndUpdate(request.params.id, { name, number }, { new: true, runValidators: true, context: 'query' })
         .then(updatedPerson => {
             response.json(updatedPerson)
         })
@@ -99,7 +82,10 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ erorr: error.message })
     }
+
     next(error)
 }
 
